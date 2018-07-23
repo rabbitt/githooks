d6935fbaba01e4634f9ee27aa805d0e9a24f426b
Carl P. Corliss <carl.corliss@finalsite.com>
2018-07-23 17:21:38 -0400

    bump version

--- lib/githooks/version.rb
+++ lib/githooks/version.rb
@@ -18,5 +18,5 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 =end
 
 module GitHooks
-  VERSION = '1.6.1' unless defined? VERSION
+  VERSION = '1.6.2' unless defined? VERSION
 end
